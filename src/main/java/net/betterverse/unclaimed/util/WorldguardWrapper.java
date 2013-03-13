20f538b427708f962d716dbb9e4f322ce8bea9cb
Travis Ralston <travpc@gmail.com>
2013-03-12 20:39:42 -0600

    Attempt to fix WG chunk issues. Should fix PLUGIN-17

--- src/main/java/net/betterverse/unclaimed/util/WorldguardWrapper.java
+++ src/main/java/net/betterverse/unclaimed/util/WorldguardWrapper.java
@@ -1,13 +1,22 @@
 package net.betterverse.unclaimed.util;
 
-import com.sk89q.worldedit.Vector;
-import com.sk89q.worldguard.bukkit.BukkitPlayer;
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+
+import com.sk89q.worldedit.BlockVector;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.UnsupportedIntersectionException;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 public class WorldguardWrapper implements ProtectionProvider {
     private WorldGuardPlugin plugin;
@@ -78,8 +87,32 @@ public class WorldguardWrapper implements ProtectionProvider {
         if (plugin == null) {
             return false;
         }
+        World world = chunk.getWorld();
+        int minChunkX = chunk.getX() << 4;
+        int minChunkZ = chunk.getZ() << 4;
+        int maxChunkX = minChunkX + 15;
+        int maxChunkZ = minChunkZ + 15;
 
-        return plugin.getRegionManager(chunk.getWorld()).getApplicableRegions(chunk.getBlock(0, 0, 0).getLocation()).size() > 0;
+        int worldHeight = world.getMaxHeight(); // Allow for heights other than default
+
+        BlockVector minChunk = new BlockVector(minChunkX, 0, minChunkZ);
+        BlockVector maxChunk = new BlockVector(maxChunkX, worldHeight, maxChunkZ);
+        ProtectedCuboidRegion chunkRegion = new ProtectedCuboidRegion("overlap-check-for-regions", minChunk, maxChunk);
+        RegionManager man = plugin.getRegionManager(chunk.getWorld());
+        Map<String, ProtectedRegion> regions = man.getRegions(); // key = name
+        try {
+            List<ProtectedRegion> intersects = chunkRegion.getIntersectingRegions(new ArrayList<ProtectedRegion>(regions.values()));
+            if (intersects != null && !intersects.isEmpty()) {
+                return true;
+            }
+        } catch (UnsupportedIntersectionException e) {
+            // Something that shouldn't happen
+            e.printStackTrace();
+        }
+
+        return false;
+        //return plugin.getRegionManager(chunk.getWorld()).getApplicableRegions(chunk.getBlock(0, 0, 0).getLocation()).size() > 0;
+
         //Vector v = toVector();
         //return plugin.canBuild(null, null)
         /*for (int x = 0; x < 16; x++) {
